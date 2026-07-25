7134271cd8cb777615b37b9b73353529f3f13f62
uwutut <109206274+uwutut@users.noreply.github.com>
2026-07-25 19:14:58 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,221 @@
+const updates = [
+
+
+{
+    version: "7/22",
+    date: "2026-07-22",
+    type: "패치",
+    title: "시랼 출시 및 업데이트 내용",
+    new: true,
+    content: [
+        {
+            title: "콘텐츠 확장",
+            text: "[일반] 모험 확장 (346F ~ 350F)<br>월요일 [인페르노] 모험 확장 (346F ~ 350F)<br>하드 모험 스테이지 점프 기능 추가<br>혼돈의 시련, 속성시련 시련 확장 (No. 2150)"
+        },
+        {
+            title: "이벤트",
+            text: "[3주년] 논스톱 SSR+ 한계돌파 소환 (마고까지)<br>하진성의 FUG 모집<br>참참참 이벤트<br>성장 교환 (넨 네야까지)<br>연합 함대전 시즌 10<br>연합 협동 미션<br>진검승부 시즌 9<br>타워 레이스"
+        },
+        {
+            title: "의상",
+            text: "신규 의상 등장 ( [붉은 등불] 시랼, [갯뱀의 자손] 리랼)<br>복각 의상 등장 ( [FUG의 간부] 하유라)"
+        },
+        {
+            title: "기타 내용",
+            text: "[가시] 비올레 - 레볼루션 스킬 밸런스 조정 (버프)<br>랭커 레이스 입장 조건 변경 및 보상 변경<br>  ➔ [인페르노]완등 시 입장 가능<br>"
+        },
+        {
+            title:"시랼 스킬셋",
+            text:"궁극기 화무십일홍 - 가장 멀리 있는 적군 뒤로 순간이동한 뒤 부채를 휘둘러 때린 후 넉백시킨다(스킬 사용 중 상태이상 면역).<br>액티브 비선영수 - 가장 멀리 있는 적군에게 부채를 던져 경로상 적군에게 대미지를 입힌 뒤 대상 후방으로 순간이동하며 부채를 휘둘러 대미지를 입히며 넉백시킨다(스킬 사용 중 상태이상 면역).<br>액티브 몽환향 - 연기를 일으켜 범위 내 적군의 명중과 회피를 감소시킴. 추가로 연기가 지속되는 동안 자신은 은신하며 매초마다 에너지를 회복한다.<br>패시브 - 자신의 스킬 공격이 적중한 적군을 출혈시켜 매초 공격력 N%의 대미지와 대상 최대 체력 N%의 대미지를 입힌다. (중첩 제한 없음) 전장에 출혈된 적군이 N명 이상 있다면 적군의 공격을 무조건 회피한다.",
+            images:["images/16.png"]
+        },
+        {
+            title: "레볼루션 능력",
+            text:"4단계: [몽환향] 스킬이 강화되어 연기가 지속되는 동안 공격력, 급속이 증가하고 매초마다 자신의 액티브 쿨타임이 감소한다. <br>5단계: [소중유검] 스킬이 강화되어 전투 시작 시 가장 멀리 있는 적군 N명에게 출혈 디버프를 건다. 추가로 출혈 상태의 적군에게 대미지를 입힐 때마다 공격력이 증가한다. (최대 N중첩) 자신이 출혈 시킨 적군의 체력 회복을 차단한다.",
+            images:["images/17.png"]
+        },
+        {
+            title: "시동무기 능력",
+            text: "LV 1 - [비선영수] 스킬 사용 시 마법 관통률이 증가한다.<br>LV 2 - [몽환향] 스킬이 강화되어 연기 안에 있는 적군의 방어력을 감소시킨다.<br>LV 3 - [몽환향] 스킬이 강화되어 연기 안에 있는 적군의 마법 저항률을 감소시킨다.<br> LV 4 - [몽환향] 스킬이 강화되어 연기 안에 있는 적군의 급속을 감소시킨다. ",
+            images:["images/18.png"]
+        }
+    ]
+},
+
+{
+version:"7/20",
+date:"2026-07-20",
+type:"패치",
+new:false,
+title:"라이브 방송",
+
+content:[
+    {
+        title:"쿠폰",
+        text:"TOG3YRFESTA - 성장 재화 선택상자 6H * 1000<br> 3YRTOGETHER - 마스터키 1000개<br>THANKUAD3YR - 빛레볼 1만개 <br>TOG3YRGIFT - 초록레볼 2천개<br>SHLTOG3YR - 전설 부유선 제작도 1개<br>TOG3YRUPDATE - 빛레볼 1만개<br>TOGPDGIFT - 자유의 교범서 20개<br> TOGLOVEU - 마스터키 1천개<br> TOGNEWCOS - SRR+ 탑의 축복 돌파석 60개<br> TOG3YRNEW - 암티 100장<br>TOG3YRTHANKU - 기원티켓 50장"
+    
+    },
+    {
+        title:"일정",
+        text:"앞으로 이벤트에 관한 일정 설명입니다.",
+        images:["images/1.png"]
+    },
+    {
+        title:"이벤트",
+        text:"논스톱 이벤트(마고까지)<br><br>성장교환(넨네야까지) + 레볼루션 초기화 티켓<br><br>스승과 제자 이벤트 시작",
+        images:["images/2.jpg", 
+                "images/3.png",
+                "images/4.png",
+                "images/5.png"]
+    },
+{
+    title:"신규 동료 - 시랼",
+    text:"전투 시작 후 가장 멀리 있는 적군에게 출혈 디버프 부여<br><br>패시브 - 적군 출혈 버프가 있으면 반드시 회피 부여<br><br>액티브1 - 적군 명중·회피 감소 및 본인 은신 + 에너지 회복<br><br>엑티브2 -  멀리 잇는 적에게 부채를 날려 순간이동 후 대상을 넉백(모든 스킬공격에 출혈효과)궁 - 가장 멀리잇는 적군에게 이동 후 넉백 및 중앙으로 모으기<br><br>출혈딜 - 체퍼뎀",
+    
+    images:[
+        "images/6.png"
+    ]
+},
+
+{
+        title:"신규 가주",
+        text:"신규 가주 연 이랑입니다.",
+        images:["images/7.png"]
+},
+{
+        title:"가시 비올레 버프",
+        text:"7/22부터 적용입니다.",
+        images:["images/8.png"]
+},
+{
+        title:"PC클라이언트 제작중",
+        text:"보안상 이유 때문에 접속 할때마다 로그인 필요.",
+        images:["images/9.png"]
+},
+{
+        title:"7/22 업데이트",
+        text:"편의성, 랭커 레이스 조건 개편<br><br>연합 합동 미션에 빛레볼, 교범서 배정<br><br>콜로세움 아레나에 마스터키, 시동 주사위 배정",
+        images:["images/10.png", "images/11.png"]
+},
+{
+        title:"8/5 업데이트",
+        text:"풀노드 해야지 이용 가능<br><br> 관통/저항, 명중/회피, 효적/효저 추가 강화 가능",
+        images:["images/12.png"]
+},
+{
+        title:"8/19 업데이트",
+        text:"녹장어 추가",
+        images:["images/13.png"]
+},
+{
+        title:"9/2 업데이트",
+        text:"트메 복각 + 버프(개구리 딜, 생존력 증가)<br><br>신규 부유선 및 시동무기 개발중",
+        images:["images/14.png"]
+},
+{
+        title:"사전 QNA 답변",
+        text:"3주년 업데이트부터 지게나 보스 딜 안 들어가는 구간 완화 + 보상 획득량 상향<br><br>기가보스 참여도 비중 완화 및 보상 상향<br><br>레볼 및 코어 상시 초기화는 도입 X <br><br>인페르노 및 밥솥 코어 완화<br><br>코어 통합 안해줄거임<br><br>밥솥에 자동 재도전 구현 안 할 계획<br><br>가주 기원 좀 더 자주 할 계획<br><br>SSR 기원은 추가할 예정X<br><br>태생의 보주 교범서 획득처 증가시킬 계획<br><br>강화된 시무 분해기능 추가 예정(8/19)<br><br>신규 시동무기 추기(9/30)<br><br>부유선 천장 시스템 도입은 하지 않을 것이지만, 부유선 '확정 상자' 추가 예정<br><br> 켈헬람, 리랼 버프 예정<br><br>점령전 구조 및 보상 개편, 오락실 보상 상향<br><br>인페르노 관리자 수 낮추고 보상 압축 기획 중<br><br>고급 결정체로 마스터키, 레볼루션 재화 교환 추가<br><br>발판 버프? 하는듯<br><br>연합 꾸미기 추가<br><br>상태이상 버프, 디버프 가시성 상향<br><br>코어가 전투력 표기에 더 잘 반영되도록 패치",
+},
+],
+
+
+}
+
+];
+
+const list=document.getElementById("list");
+
+function draw(){
+
+list.innerHTML="";
+
+const keyword=document
+.getElementById("search")
+.value
+.toLowerCase();
+
+const filter=document
+.getElementById("filter")
+.value;
+
+updates.forEach(update=>{
+
+const text=
+JSON.stringify(update).toLowerCase();
+
+if(!text.includes(keyword)) return;
+
+if(filter!="전체"&&filter!=update.type) return;
+
+const card=document.createElement("div");
+
+card.className="card";
+
+card.innerHTML=`
+
+<div class="head">
+
+<div class="left">
+
+<div class="version">${update.version}</div>
+
+<div>${update.title}</div>
+
+<div class="date">${update.date}</div>
+
+<div class="type">${update.type}</div>
+
+${update.new?'<div class="badge">NEW</div>':""}
+
+</div>
+
+<div>▼</div>
+
+</div>
+
+
+<div class="content">
+
+${update.content.map(item => `
+    <div class="update-section">
+
+        <h3>${item.title}</h3>
+
+        <p>${item.text}</p>
+
+        ${
+            item.images
+            ? item.images.map(img =>
+                `<img src="${img}" class="update-image">`
+              ).join("")
+            : ""
+        }
+
+    </div>
+`).join("")}
+
+`;
+
+card.querySelector(".head").onclick=()=>{
+
+const c=card.querySelector(".content");
+
+c.style.display=
+c.style.display=="block"
+?"none":"block";
+
+};
+
+list.appendChild(card);
+
+});
+
+
+
+}
+
+draw();
+
+search.oninput=draw;
+
+filter.onchange=draw;
